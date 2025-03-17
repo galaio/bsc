533ff65ae22fab7d5fc7c09fc6943569947dd9f4
galaio <galaio@users.noreply.github.com>
2025-03-17 14:41:36 +0800

    bsc: add new msg definition;

--- eth/protocols/bsc/protocol.go
+++ eth/protocols/bsc/protocol.go
@@ -1,7 +1,10 @@
 package bsc
 
 import (
+	"bytes"
 	"errors"
+	"github.com/ethereum/go-ethereum/common"
+	"math/big"
 
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/rlp"
@@ -28,8 +31,12 @@ var protocolLengths = map[uint]uint64{Bsc1: 2}
 const maxMessageSize = 10 * 1024 * 1024
 
 const (
-	BscCapMsg = 0x00 // bsc capability msg used upon handshake
-	VotesMsg  = 0x01
+	BscCapMsg                  = 0x00 // bsc capability msg used upon handshake
+	VotesMsg                   = 0x01
+	GetBlocksByRangeMsg        = 0x02 // it can request (Head-n, Head] range blocks from remote peer
+	RangeBlocksMsg             = 0x03 // the replied blocks from remote peer
+	GetAuthorizationForPeerMsg = 0x04 // sentry node can request authorization from validator to join the public network
+	PeerAuthorizationMsg       = 0x05 // the replied auth from validator
 )
 
 var defaultExtra = []byte{0x00}
@@ -54,13 +61,99 @@ type BscCapPacket struct {
 	Extra           rlp.RawValue // for extension
 }
 
+func (*BscCapPacket) Name() string { return "BscCap" }
+func (*BscCapPacket) Kind() byte   { return BscCapMsg }
+
+const (
+	CapExtraDefaultVer      = 0
+	CapExtraWithIdentityVer = 1
+)
+
+type ExtraWithIdentity struct {
+	list []*NodeIdentity
+}
+
+type NodeIdentity struct {
+	ChainID     uint64
+	ForkID      [4]byte
+	GenesisHash common.Hash
+	PeerID      string
+	NodeVersion string
+	Extend      []byte
+	CreateTime  uint64   // a UTC unix timestamp when it creates
+	V, R, S     *big.Int // signature values
+}
+
+func EncodeCapExtra(ver byte, val interface{}) ([]byte, error) {
+	if val == nil {
+		return nil, errors.New("input nil value")
+	}
+	var buf bytes.Buffer
+	buf.WriteByte(ver)
+	if err := rlp.Encode(&buf, val); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func DecodeExtraWithIdentity(enc []byte) (*ExtraWithIdentity, error) {
+	if len(enc) <= 1 {
+		return nil, errors.New("too short TxDAG bytes")
+	}
+
+	if enc[0] != CapExtraWithIdentityVer {
+		return nil, errors.New("wrong ver")
+	}
+
+	extra := new(ExtraWithIdentity)
+	if err := rlp.DecodeBytes(enc[1:], extra); err != nil {
+		return nil, err
+	}
+	return extra, nil
+}
+
 // VotesPacket is the network packet for votes record.
 type VotesPacket struct {
 	Votes []*types.VoteEnvelope
 }
 
-func (*BscCapPacket) Name() string { return "BscCap" }
-func (*BscCapPacket) Kind() byte   { return BscCapMsg }
-
 func (*VotesPacket) Name() string { return "Votes" }
 func (*VotesPacket) Kind() byte   { return VotesMsg }
+
+type GetAuthorizationForPeerPacket struct {
+	RequestId   uint64
+	ChainID     uint64
+	ForkID      [4]byte
+	GenesisHash common.Hash
+	PeerID      string
+	NodeVersion string
+	Extend      []byte
+}
+
+func (*GetAuthorizationForPeerPacket) Name() string { return "GetAuthorizationForPeer" }
+func (*GetAuthorizationForPeerPacket) Kind() byte   { return GetAuthorizationForPeerMsg }
+
+type PeerAuthorizationPacket struct {
+	RequestId uint64
+	Auth      *NodeIdentity
+}
+
+func (*PeerAuthorizationPacket) Name() string { return "PeerAuthorization" }
+func (*PeerAuthorizationPacket) Kind() byte   { return PeerAuthorizationMsg }
+
+type GetBlocksByRangePacket struct {
+	RequestId   uint64
+	StartHeight uint64 // The start block height expected to be obtained from
+	Count       uint64 // Get the number of blocks from the start
+}
+
+func (*GetBlocksByRangePacket) Name() string { return "GetBlocksByRange" }
+func (*GetBlocksByRangePacket) Kind() byte   { return GetBlocksByRangeMsg }
+
+type RangeBlocksPacket struct {
+	RequestId uint64
+	Blocks    []*types.Block
+}
+
+func (*RangeBlocksPacket) Name() string { return "RangeBlocks" }
+func (*RangeBlocksPacket) Kind() byte   { return RangeBlocksMsg }
